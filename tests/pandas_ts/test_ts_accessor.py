24e689dd2684b9ff4bbec30d7f522c57d15c5743
Konstantin Malanchev <hombit@gmail.com>
2024-02-06 13:32:00 -0500

    MutableMapping for TsAccessor

--- tests/pandas_ts/test_ts_accessor.py
+++ tests/pandas_ts/test_ts_accessor.py
@@ -7,20 +7,7 @@ import pytest
 from numpy.testing import assert_array_equal
 from pandas.testing import assert_frame_equal, assert_series_equal
 
-
-@pytest.mark.skip("It is really tricky to test it: some other test may import pandas-ts before this one")
-def test_ts_accessor_not_registered():
-    struct_array = pa.StructArray.from_arrays(
-        arrays=[
-            pa.array([np.array([1, 2, 3]), np.array([1.0, 2.0, 1.0])]),
-            pa.array([np.array([4, 5, 6]), np.array([3.0, 4.0, 5.0])]),
-        ],
-        names=["a", "b"],
-    )
-    series = pd.Series(struct_array, dtype=pd.ArrowDtype(struct_array.type), index=[0, 1])
-
-    with pytest.raises(AttributeError):
-        _accessor = series.ts
+from pandas_ts import TsDtype
 
 
 def test_ts_accessor_registered():
@@ -31,32 +18,9 @@ def test_ts_accessor_registered():
         ],
         names=["a", "b"],
     )
-    series = pd.Series(struct_array, dtype=pd.ArrowDtype(struct_array.type), index=[0, 1])
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
 
     _accessor = series.ts
-
-
-def test_no_ts_accessor_for_non_struct():
-    array = pa.array([np.array([1, 2, 3]), np.array([4, 5, 6])])
-    series = pd.Series(array, dtype=pd.ArrowDtype(array.type), index=[0, 1])
-
-    with pytest.raises(AttributeError):
-        _accessor = series.ts
-
-
-def test_no_ts_accessor_for_wrong_struct_fields():
-    struct_array = pa.StructArray.from_arrays(
-        arrays=[
-            pa.array([np.array([1, 2, 3]), np.array([1.0, 2.0, 1.0])]),
-            pa.array([np.array([4, 5, 6]), np.array([3.0, 4.0, 5.0])]),
-            pa.array([-6, -7]),
-        ],
-        names=["a", "b", "c"],
-    )
-    series = pd.Series(struct_array, dtype=pd.ArrowDtype(struct_array.type), index=[0, 1])
-
-    with pytest.raises(AttributeError):
-        _accessor = series.ts
 
 
 def test_ts_accessor_to_lists():
@@ -67,7 +31,7 @@ def test_ts_accessor_to_lists():
         ],
         names=["a", "b"],
     )
-    series = pd.Series(struct_array, dtype=pd.ArrowDtype(struct_array.type), index=[0, 1])
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
 
     lists = series.ts.to_lists()
 
@@ -96,7 +60,7 @@ def test_ts_accessor_to_flat():
         ],
         names=["a", "b"],
     )
-    series = pd.Series(struct_array, dtype=pd.ArrowDtype(struct_array.type), index=[0, 1])
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
 
     flat = series.ts.to_flat()
 
@@ -132,9 +96,92 @@ def test_ts_accessor_fields():
         ],
         names=["a", "b"],
     )
-    series = pd.Series(struct_array, dtype=pd.ArrowDtype(struct_array.type), index=[0, 1])
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
 
-    assert_array_equal(series.ts.fields, pd.Index(["a", "b"]))
+    assert_array_equal(series.ts.fields, ["a", "b"])
+
+
+def test_ts_accessor_flat_length():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    assert series.ts.flat_length == 6
+
+
+def test_set_flat_field():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    series.ts.set_flat_field("a", np.array(["a", "b", "c", "d", "e", "f"]))
+
+    assert_series_equal(
+        series.ts["a"],
+        pd.Series(
+            data=[["a", "b", "c"], ["d", "e", "f"]],
+            index=[0, 1],
+            name="a",
+            dtype=pd.ArrowDtype(pa.list_(pa.string())),
+        ),
+    )
+
+
+def test_set_list_field():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    series.ts.set_list_field("c", [["a", "b", "c"], ["d", "e", "f"]])
+
+    assert_series_equal(
+        series.ts["c"],
+        pd.Series(
+            data=[["a", "b", "c"], ["d", "e", "f"]],
+            index=[0, 1],
+            name="c",
+            dtype=pd.ArrowDtype(pa.list_(pa.string())),
+        ),
+    )
+
+
+def test_delete_field():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    a = series.ts.delete_field("a")
+
+    assert_array_equal(series.ts.fields, ["b"])
+    assert_series_equal(
+        a,
+        pd.Series(
+            [np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])],
+            dtype=pd.ArrowDtype(pa.list_(pa.float64())),
+            index=[0, 1],
+            name="a",
+        ),
+    )
 
 
 def test_ts_accessor___getitem__():
@@ -145,7 +192,7 @@ def test_ts_accessor___getitem__():
         ],
         names=["a", "b"],
     )
-    series = pd.Series(struct_array, dtype=pd.ArrowDtype(struct_array.type), index=[0, 1])
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
 
     assert_series_equal(
         series.ts["a"],
@@ -165,3 +212,127 @@ def test_ts_accessor___getitem__():
             name="b",
         ),
     )
+
+
+def test_ts_accessor___setitem___with_flat():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    series.ts["a"] = np.array(["a", "b", "c", "d", "e", "f"])
+
+    assert_series_equal(
+        series.ts["a"],
+        pd.Series(
+            data=[["a", "b", "c"], ["d", "e", "f"]],
+            index=[0, 1],
+            name="a",
+            dtype=pd.ArrowDtype(pa.list_(pa.string())),
+        ),
+    )
+
+
+def test_ts_accessor___setitem___with_list():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    series.ts["c"] = [["a", "b", "c"], ["d", "e", "f"]]
+
+    assert_series_equal(
+        series.ts["c"],
+        pd.Series(
+            data=[["a", "b", "c"], ["d", "e", "f"]],
+            index=[0, 1],
+            name="c",
+            dtype=pd.ArrowDtype(pa.list_(pa.string())),
+        ),
+    )
+
+
+def test_ts_accessor___setited___raises_for_ambiguous_lengths_1():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array(
+                [
+                    np.array(
+                        [
+                            1.0,
+                        ]
+                    ),
+                    np.array([2.0]),
+                ]
+            ),
+            pa.array([-np.array([6.0]), -np.array([5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    with pytest.raises(ValueError):
+        series.ts["c"] = ["a", "b", "c"]
+
+
+def test_ts_accessor___setited___raises_for_ambiguous_lengths_2():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0]), np.array([])]),
+            pa.array([-np.array([6.0, 5.0]), -np.array([])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    with pytest.raises(ValueError):
+        series.ts["c"] = ["a", "b", "c"]
+
+
+def test_ts_accessor___delitem__():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    del series.ts["a"]
+
+    assert_array_equal(series.ts.fields, ["b"])
+
+
+def test_ts_accessor___iter__():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    assert_array_equal(list(series.ts), ["a", "b"])
+
+
+def test_ts_accessor___len__():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    series = pd.Series(struct_array, dtype=TsDtype(struct_array.type), index=[0, 1])
+
+    assert len(series.ts) == 2
--- tests/pandas_ts/test_ts_ext_array.py
+++ tests/pandas_ts/test_ts_ext_array.py
@@ -153,3 +153,173 @@ def test___iter__():
     # Check last df only
     df = list(series)[-1]
     assert_frame_equal(df, pd.DataFrame({"a": np.array([1.0, 2.0, 1.0]), "b": -np.array([3.0, 4.0, 5.0])}))
+
+
+def test_field_names():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0])]),
+        ],
+        names=["a", "b"],
+    )
+    ext_array = TsExtensionArray(struct_array)
+
+    assert ext_array.field_names == ["a", "b"]
+
+
+def test_flat_length():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+        ],
+        names=["a", "b"],
+    )
+    ext_array = TsExtensionArray(struct_array)
+
+    assert ext_array.flat_length == 7
+
+
+def test_set_flat_field_new_field_scalar():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+        ],
+        names=["a", "b"],
+    )
+    ext_array = TsExtensionArray(struct_array)
+
+    ext_array.set_flat_field("c", "YES")
+
+    desired_struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+            pa.array([["YES"] * 3, ["YES"] * 4]),
+        ],
+        names=["a", "b", "c"],
+    )
+    desired = TsExtensionArray(desired_struct_array)
+
+    assert_series_equal(pd.Series(ext_array), pd.Series(desired))
+
+
+def test_set_flat_field_replace_field_array():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 3.0, 4.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+        ],
+        names=["a", "b"],
+    )
+    ext_array = TsExtensionArray(struct_array)
+
+    ext_array.set_flat_field("b", [True, False, True, False, True, False, True])
+
+    desired_struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 3.0, 4.0])]),
+            pa.array([np.array([True, False, True]), np.array([False, True, False, True])]),
+        ],
+        names=["a", "b"],
+    )
+    desired = TsExtensionArray(desired_struct_array)
+
+    assert_series_equal(pd.Series(ext_array), pd.Series(desired))
+
+
+def test_set_list_field_new_field():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+        ],
+        names=["a", "b"],
+    )
+    ext_array = TsExtensionArray(struct_array)
+
+    ext_array.set_list_field("c", [["x", "y", "z"], ["x1", "x2", "x3", "x4"]])
+
+    desired_struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+            pa.array([np.array(["x", "y", "z"]), np.array(["x1", "x2", "x3", "x4"])]),
+        ],
+        names=["a", "b", "c"],
+    )
+    desired = TsExtensionArray(desired_struct_array)
+
+    assert_series_equal(pd.Series(ext_array), pd.Series(desired))
+
+
+def test_set_list_field_replace_field():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+        ],
+        names=["a", "b"],
+    )
+    ext_array = TsExtensionArray(struct_array)
+
+    ext_array.set_list_field("b", [["x", "y", "z"], ["x1", "x2", "x3", "x4"]])
+
+    desired_struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([np.array(["x", "y", "z"]), np.array(["x1", "x2", "x3", "x4"])]),
+        ],
+        names=["a", "b"],
+    )
+    desired = TsExtensionArray(desired_struct_array)
+
+    assert_series_equal(pd.Series(ext_array), pd.Series(desired))
+
+
+def test_delete_field():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+            pa.array([np.array(["x", "y", "z"]), np.array(["x1", "x2", "x3", "x4"])]),
+        ],
+        names=["a", "b", "c"],
+    )
+    ext_array = TsExtensionArray(struct_array)
+
+    ext_array.delete_field("c")
+
+    desired_struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+        ],
+        names=["a", "b"],
+    )
+    desired = TsExtensionArray(desired_struct_array)
+
+    assert_series_equal(pd.Series(ext_array), pd.Series(desired))
+
+
+def test_delete_last_field_raises():
+    struct_array = pa.StructArray.from_arrays(
+        arrays=[
+            pa.array([np.array([1.0, 2.0, 3.0]), np.array([1.0, 2.0, 1.0, 2.0])]),
+            pa.array([-np.array([4.0, 5.0, 6.0]), -np.array([3.0, 4.0, 5.0, 6.0])]),
+            pa.array([np.array(["x", "y", "z"]), np.array(["x1", "x2", "x3", "x4"])]),
+        ],
+        names=["a", "b", "c"],
+    )
+    ext_array = TsExtensionArray(struct_array)
+
+    ext_array.delete_field("a")
+    assert ext_array.field_names == ["b", "c"]
+
+    ext_array.delete_field("c")
+    assert ext_array.field_names == ["b"]
+
+    with pytest.raises(ValueError):
+        ext_array.delete_field("b")
