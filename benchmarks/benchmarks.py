3b6408217a47d4dce995414ae04c5b8efeb33bef
Konstantin Malanchev <hombit@gmail.com>
2024-02-12 10:02:05 -0500

    Initial benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,14 +3,57 @@
 For more information on writing benchmarks:
 https://asv.readthedocs.io/en/stable/writing_benchmarks.html."""
 
-from time import sleep
+
+import numpy as np
+import pandas as pd
+from pandas_ts.packer import pack_flat
 
 
-def time_computation():
-    """Time computations are prefixed with 'time'."""
-    sleep(1e-3)
+def dummy_flat_df(n_object, n_per_object):
+    n_flat = n_object * n_per_object
+    return pd.DataFrame(
+        {
+            "x": np.arange(n_flat),
+            "y": np.linspace(0, 1, n_flat),
+        },
+        index=np.tile(np.arange(n_object), n_per_object),
+    )
 
 
-def mem_list():
-    """Memory computations are prefixed with 'mem' or 'peakmem'."""
-    sleep(1e-3)
+class PackAndApplyLength:
+    def setup(self):
+        self.flat = dummy_flat_df(10_000, 100)
+
+    def time_packed(self):
+        series = pack_flat(self.flat)
+        _result = series.apply(len)
+
+    def time_groupby(self):
+        _result = self.flat.groupby(level=0).apply(len)
+
+
+class PackAndApplyLengthFiveTimes:
+    def setup(self):
+        self.flat = dummy_flat_df(10_000, 100)
+
+    def time_packed(self):
+        series = pack_flat(self.flat)
+        for _ in range(5):
+            _result = series.apply(len)
+
+    def time_groupby(self):
+        for _ in range(5):
+            _result = self.flat.groupby(level=0).apply(len)
+
+    def time_sort_groupby(self):
+        sorted = self.flat.sort_index(kind="stable")
+        for _ in range(5):
+            _result = sorted.groupby(level=0).apply(len)
+
+
+class Pack:
+    def setup(self):
+        self.flat = dummy_flat_df(10_000, 100)
+
+    def time_pack_flat(self):
+        _result = pack_flat(self.flat)
